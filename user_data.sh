3c0c83f807e5e633a64976b0bfea34e6a3ab045f
varundinesh88-lgtm <varundinesh88@gmail.com>
2025-10-27 08:56:24 +0000

    Updated user_data.sh and redeployed successful 3-tier architecture

--- user_data.sh
+++ user_data.sh
@@ -2,5 +2,5 @@
 sudo yum update -y
 sudo amazon-linux-extras install nginx1 -y
 sudo systemctl enable nginx
-echo "<h1>Terraform Stage3 Fresh ASG Instance</h1>" | sudo tee /usr/share/nginx/html/index.html
+echo "<h1>AWS 3-Tier Architecture Deployment Successful!</h1>" | sudo tee /usr/share/nginx/html/index.html
 sudo systemctl start nginx
